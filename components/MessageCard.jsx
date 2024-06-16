74e22b0632cad1b55d8c7ac2a8efad3ee16ac6f8
gjorgievskidejan91 <gjorgievskidejan91@gmail.com>
2024-06-16 17:17:57 +0200

    Prepairing for production fixed message

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -7,23 +7,29 @@ import {
   CardTitle,
 } from "@/app/components/ui/card";
 import { fetchMessages } from "@/app/lib/data";
-const MessageCard = async () => {
-  const messages = await fetchMessages();
 
-  if (!messages) {
-    return <h2>No message data</h2>;
+const MessageCard = async () => {
+  let messages = [];
+
+  try {
+    messages = await fetchMessages();
+  } catch (error) {
+    console.error("Error fetching messages:", error);
+    return <h2>Error fetching message data</h2>;
   }
 
   return (
-    // <h1>Hello</h1>
     <>
+      {messages.length === 0 && <h2>No message data</h2>}
       {messages.map((message, index) => (
         <Card key={index} className="shadow-lg rounded-lg overflow-hidden">
           <CardHeader>
             <CardTitle>{message.name}</CardTitle>
-            <h2>
-              {message.carOfInterest.make} {message.carOfInterest.model}
-            </h2>
+            {message.carOfInterest && ( // Check if carOfInterest is not null
+              <h2>
+                {message.carOfInterest.make} {message.carOfInterest.model}
+              </h2>
+            )}
           </CardHeader>
           <CardContent>
             <p>{message.message}</p>
